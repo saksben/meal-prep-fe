29439926cacae601a28a52e94885e814e2adb0a7
Benjamin Saks <saksben2@gmail.com>
2024-10-06 15:47:35 -0700

    create basic user page

---
+++ src/app/users/page.tsx
@@ -0,0 +1,17 @@
+// Eventually have a daily macro calculator
+
+const Users = () => {
+  return (
+  <div className='p-[2rem]'>
+    <h1 className='text-xl font-semibold text-blue-400'>Benjamin's Target Daily Nutrition</h1>
+    <p>1750 Calories</p>
+    <div className='mt-2'>
+        <p>133 - 173g Carbs</p>
+        <p>35 - 55g Fat</p>
+        <p>160 - 200g Protein</p>
+    </div>
+  </div>
+);
+};
+
+export default Users;
